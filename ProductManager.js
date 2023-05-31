e3fb8f77f0453f6e7dd0cbc475b2830255559d09
vickyyg <viicjygaida@gmail.com>
2023-05-30 23:18:46 -0300

    update

--- ProductManager.js
+++ ProductManager.js
@@ -1,97 +1,83 @@
-import { promises as fs } from 'fs';
+
+import {promises as fs} from "fs"
 
 
-class ProductManager {
-  constructor() {
-    this.patch = './productos.txt';
-    this.products = [];
-  }
-  static id = 0
-
-  addProduct = async (title, description, price, thumbnail, code, stock) => {
-    
-    ProductManager.id++;
-
-    let newProduct = {
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-        id: ProductManager.id
-        
+export default class ProductManager {
+    constructor(){
+        this.patch = "./productos.txt"
+        this.products= []
     }
 
-    this.products.push(newProduct);
+    static id = 0 
 
-   //JSON.stringify para pasar newProduct a string
-    await fs.writeFile(this.patch, JSON.stringify(this.products));
-  };
+    addProduct = async (title, description, price, thumbnail, code, stock) =>{
 
-  readProducts = async () => {
-    let respuesta = await fs.readFile(this.patch, "utf-8");
-     //JSON.parse para pasar a objeto
-    return JSON.parse(respuesta);
-  }
+        this.id++;
 
-  getProducts = async () => {
-    let respuesta2 = await this.readProducts();
-    return console.log(respuesta2);
-  }
+        let newProduct = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+            id: this.id,
+        }
+
+        this.products.push(newProduct)
+
+        await fs.writeFile(this.patch, JSON.stringify(this.products));
+    };
+
+    readProducts = async () => {
+        let respuesta = await fs.readFile(this.patch, "utf-8")
+        return JSON.parse(respuesta)
+    }
+    
+    getProducts = async () => {
+        let respuesta2 = await this.readProducts()
+        return console.log(respuesta2)
+    }
 
     getProductsById = async (id) => {
-    
-        let respuesta3 = await this.readProducts();
-      if(!respuesta3.find(product => product.id === id)) {
-        console.log("No se encuentran los productos")
-      } else {
-        console.log(respuesta3.find(product => product.id === id))
-      }
-
-       
-    };
+        let respuesta3 = await this.readProducts()
+        if(!respuesta3.find(product => product.id === id)){
+            console.log("No se encuentra el producto")
+        } else{
+            console.log(respuesta3.find(product => product.id === id));
+        }
+    }
 
     deleteProductsById = async (id) => {
-        let respuesta3 = await this.readProducts();
-        let productFilter = respuesta3.filter(products => products.id != id);
+        let respuesta3 = await this.readProducts()
+        let productFilter = respuesta3.filter(products => products.id != id)
         await fs.writeFile(this.patch, JSON.stringify(productFilter));
-        console.log("Producto eliminado");
-    };
-    
+        
+        console.log("Se ha encontrado el producto")
+    }
+
     updateProducts = async ({id, ...producto}) => {
-    await this.deleteProductsById(id);
-    let productoId = await this.readProducts();
-   
+        await this.deleteProductsById(id);
+        let oldProduct = await this.readProducts()
+        let modifiedProduct = [{ ...producto, id }, ...oldProduct];
+        await fs.writeFile(this.patch, JSON.stringify(modifiedProduct));
+      
+    }
 
-   let modProducts = [
-    { ...producto, id},
-    ...productoId
-         
-    ] 
-    await fs.writeFile(this.patch, JSON.stringify(modProducts));
-
-    };
 }
 
-const productos = new ProductManager
 
 
 
 /* productos.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
-productos.addProduct("producto prueba2", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25); 
-productos.addProduct("producto prueba3", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);   */
+productos.addProduct("producto prueba2", "Este es un producto prueba", 250, "Sin imagen", "abc123", 25); 
+productos.addProduct("producto prueba3", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);  
+ productos.addProduct("producto prueba4", "Este es un producto prueba", 1000, "Sin imagen", "abc123", 25);  
+ productos.addProduct("producto prueba5", "Este es un producto prueba", 2900, "Sin imagen", "abc123", 25);  
+ productos.addProduct("producto prueba6", "Este es un producto prueba", 300, "Sin imagen", "abc123", 25);  
+ productos.addProduct("producto prueba7", "Este es un producto prueba", 123, "Sin imagen", "abc123", 25);  
+ productos.addProduct("producto prueba8", "Este es un producto prueba", 9820, "Sin imagen", "abc123", 25);  
+ productos.addProduct("producto prueba9", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);  
+ productos.addProduct("producto prueba10", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);  */
 
-//productos.getProducts();
 
-//productos.getProductsById(1);
-
-//productos.deleteProductsById(1);
-
-productos.updateProducts(  {title: 'producto prueba3',
-description: 'Este es un producto prueba',
-price: 400,
-thumbnail: 'Sin imagen',
-code: 'abc123',
-stock: 25,
-id: 3})
